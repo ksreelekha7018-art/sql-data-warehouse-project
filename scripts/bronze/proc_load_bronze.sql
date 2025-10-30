5fffff06d350df402c85b0ae08f9cb2e2440b823
ksreelekha7018-art <ksreelekha7018@gmail.com>
2025-10-30 16:11:10 -0500

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,125 @@
+CREATE or ALTER PROCEDURE bronze.load_bronze as
+BEGIN 
+     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+     BEGIN TRY  
+        SET @start_time = GETDATE();
+        PRINT '==========================================';
+        PRINT 'Loading bronce Layer';
+        PRINT '==========================================';
+
+        PRINT '-------------------------------------------';
+        PRINT 'Loading CRM Tables';
+        PRINT '--------------------------------------------';
+
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.crm_cust_info';
+
+        TRUNCATE TABLE bronze.crm_cust_info;
+
+        PRINT '>> Inserting data into bronze.crm_cust_info';
+        BULK INSERT bronze.crm_cust_info
+        FROM 'C:\Users\vamsi\OneDrive\Desktop\Sreelekha\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '-------------------------------------------------------------------------------------------';
+
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.crm_prd_info';
+        TRUNCATE TABLE bronze.crm_prd_info;
+
+        PRINT '>> Inserting data into bronze.crm_prd_info';
+        BULK INSERT bronze.crm_prd_info
+        FROM 'C:\Users\vamsi\OneDrive\Desktop\Sreelekha\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '-------------------------------------------------------------------------------------------';
+
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.crm_sales_details';
+        TRUNCATE TABLE bronze.crm_sales_details;
+
+        PRINT '>> Inserting data into bronze.crm_sales_details';
+        BULK INSERT bronze.crm_sales_details
+        FROM 'C:\Users\vamsi\OneDrive\Desktop\Sreelekha\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '-------------------------------------------------------------------------------------------';
+
+        PRINT '-------------------------------------------';
+        PRINT 'Loading ERP Tables';
+        PRINT '--------------------------------------------';
+
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.erp_cust_az12';
+        TRUNCATE TABLE bronze.erp_cust_az12;
+
+        PRINT '>> Inserting data into bronze.erp_cust_az12';
+        BULK INSERT bronze.erp_cust_az12
+        FROM 'C:\Users\vamsi\OneDrive\Desktop\Sreelekha\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '-------------------------------------------------------------------------------------------';
+
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.erp_loc_a101';
+        TRUNCATE TABLE bronze.erp_loc_a101;
+
+        PRINT '>> Inserting data into bronze.erp_loc_a101';
+        BULK INSERT bronze.erp_loc_a101
+        FROM 'C:\Users\vamsi\OneDrive\Desktop\Sreelekha\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '-------------------------------------------------------------------------------------------';
+
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+        PRINT '>> Inserting data into bronze.erp_px_cat_g1v2';
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM 'C:\Users\vamsi\OneDrive\Desktop\Sreelekha\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(second, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '-------------------------------------------------------------------------------------------';
+        SET @batch_end_time = GETDATE();
+        PRINT '>>Total Load Duration : ' + CAST(DATEDIFF(second, @batch_start_time, @batch_end_time) AS VARCHAR) + ' seconds';
+        PRINT '-------------------------------------------------------------------------------------------';
+
+     END TRY
+     BEGIN CATCH
+
+     PRINT '=====================================================';
+     PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+     PRINT '=====================================================';
+     END CATCH
+END
